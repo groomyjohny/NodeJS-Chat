9a2e36843b426b23539a1a615dc879d199819e50
groomyjohny <groomyjohny@gmail.com>
2021-02-04 14:14:14 +0500

    Added project files

---
+++ server.js
@@ -0,0 +1,42 @@
+var http = require('http');
+var Static = require('node-static');
+var WebSocketServer = new require('ws');
+
+// подключенные клиенты
+var clients = {};
+let clientAge = {};
+
+// WebSocket-сервер на порту 8081
+var webSocketServer = new WebSocketServer.Server({ port: 8081 });
+webSocketServer.on('connection', function (ws) {
+
+  var id = Math.random();
+  clients[id] = ws;
+  console.log("новое соединение " + id);
+
+  ws.on('message', function (message) {
+    console.log('получено сообщение ' + message);
+    let arr = JSON.parse(message);    
+
+    for (var key in clients) {
+      clients[key].send(message);
+    }
+  });
+
+  ws.on('close', function () {
+    console.log('соединение закрыто ' + id);
+    delete clients[id];
+    delete clientAge[id];
+  });
+});
+
+
+// обычный сервер (статика) на порту 8080
+var fileServer = new Static.Server('.');
+http.createServer(function (req, res) {
+
+  fileServer.serve(req, res);
+
+}).listen(8080);
+
+console.log("Сервер запущен на портах 8080, 8081");
