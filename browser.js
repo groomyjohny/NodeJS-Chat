db3b81120f87f72bd688acd38cac7f324158d476
groomyjohny <groomyjohny@gmail.com>
2021-02-04 16:58:49 +0500

    Identation: 2 -> 4 spaces

--- browser.js
+++ browser.js
@@ -1,29 +1,29 @@
 if (!window.WebSocket) {
-	document.body.innerHTML = 'WebSocket в этом браузере не поддерживается.';
+    document.body.innerHTML = 'WebSocket в этом браузере не поддерживается.';
 }
 
 // создать подключение
 var socket = new WebSocket("ws://localhost:8081");
 
 // отправить сообщение из формы publish
-document.forms.publish.onsubmit = function() {
-  var outgoingMessage = this.message.value;
-  let data = {nick: this.nick.value, message: outgoingMessage};
-  let msg = JSON.stringify(data);
+document.forms.publish.onsubmit = function () {
+    var outgoingMessage = this.message.value;
+    let data = { nick: this.nick.value, message: outgoingMessage };
+    let msg = JSON.stringify(data);
 
-  socket.send(msg);
-  return false;
+    socket.send(msg);
+    return false;
 };
 
 // обработчик входящих сообщений
-socket.onmessage = function(event) {
-  var incomingMessage = event.data;
-  showMessage(incomingMessage); 
+socket.onmessage = function (event) {
+    var incomingMessage = event.data;
+    showMessage(incomingMessage);
 };
 
 // показать сообщение в div#subscribe
 function showMessage(message) {
-  let data = JSON.parse(message);
-  let code = '<div class="message"><div class="message-nick">' + data.nick + '</div><div class="message-text">'+data.message + '</div></div>'
-  document.getElementById('subscribe').innerHTML += code;
+    let data = JSON.parse(message);
+    let code = '<div class="message"><div class="message-nick">' + data.nick + '</div><div class="message-text">' + data.message + '</div></div>'
+    document.getElementById('subscribe').innerHTML += code;
 }
--- server.js
+++ server.js
@@ -7,42 +7,42 @@ var mysql = require('mysql2');
 var clients = {};
 
 const sqlConnection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "chat",
-  password: ""
+    host: "localhost",
+    user: "root",
+    database: "chat",
+    password: ""
 }); 
 
 // WebSocket-сервер на порту 8081
 var webSocketServer = new WebSocketServer.Server({ port: 8081 });
 webSocketServer.on('connection', function (ws) {
 
-  var id = Math.random();
-  clients[id] = ws;
-  console.log("новое соединение " + id);
+    var id = Math.random();
+    clients[id] = ws;
+    console.log("новое соединение " + id);
 
-  ws.on('message', function (message) {
-    console.log('получено сообщение ' + message);
-    let arr = JSON.parse(message);
-    const query = "INSERT INTO messages (nick, message) VALUES (?,?)";
-    sqlConnection.query(query, [arr.nick, arr.message])
+    ws.on('message', function (message) {
+        console.log('получено сообщение ' + message);
+        let arr = JSON.parse(message);
+        const query = "INSERT INTO messages (nick, message) VALUES (?,?)";
+        sqlConnection.query(query, [arr.nick, arr.message])
 
-    for (var key in clients) {
-      clients[key].send(message);
-    }
-  });
+        for (var key in clients) {
+            clients[key].send(message);
+        }
+    });
 
-  ws.on('close', function () {
-    console.log('соединение закрыто ' + id);
-    delete clients[id];
-  });
+    ws.on('close', function () {
+        console.log('соединение закрыто ' + id);
+        delete clients[id];
+    });
 });
 
 
 // обычный сервер (статика) на порту 8080
 var fileServer = new Static.Server('.');
 http.createServer(function (req, res) {
-  fileServer.serve(req, res);
+    fileServer.serve(req, res);
 }).listen(8080);
 
 console.log("Сервер запущен на портах 8080, 8081");
