f4ad3b94cded38bfeeb91ef55331d9534c0a4af2
groomyjohny <groomyjohny@gmail.com>
2021-02-04 17:18:33 +0500

    Implemented server sending previous messages to the newly connected client

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require('http');
 var Static = require('node-static');
 var WebSocketServer = new require('ws');
 var mysql = require('mysql2');
+const { Socket } = require('dgram');
 
 // подключенные клиенты
 var clients = {};
@@ -15,11 +16,27 @@ const sqlConnection = mysql.createConnection({
 
 // WebSocket-сервер на порту 8081
 var webSocketServer = new WebSocketServer.Server({ port: 8081 });
+function sendToAll(message)
+{
+    for (var key in clients) {
+        clients[key].send(message);
+    }
+}
 webSocketServer.on('connection', function (ws) {
 
     var id = Math.random();
     clients[id] = ws;
     console.log("новое соединение " + id);
+
+    sqlConnection.query("SELECT nick, message FROM messages", (err, result, fields) => {
+        if (!err)
+        {
+            for (i in result)
+            {
+                sendToAll(JSON.stringify(result[i]));
+            }
+        }
+    });
 
     ws.on('message', function (message) {
         console.log('получено сообщение ' + message);
@@ -27,9 +44,7 @@ webSocketServer.on('connection', function (ws) {
         const query = "INSERT INTO messages (nick, message) VALUES (?,?)";
         sqlConnection.query(query, [arr.nick, arr.message])
 
-        for (var key in clients) {
-            clients[key].send(message);
-        }
+        sendToAll(message)
     });
 
     ws.on('close', function () {
