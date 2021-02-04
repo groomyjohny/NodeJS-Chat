74e071e6d5ff1275738fa4cedeecf48a6e12e741
groomyjohny <groomyjohny@gmail.com>
2021-02-04 14:24:46 +0500

    Added message SQL insert

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var http = require('http');
 var Static = require('node-static');
 var WebSocketServer = new require('ws');
+var mysql = require('mysql2');
 
 // подключенные клиенты
 var clients = {};
-let clientAge = {};
+
+const sqlConnection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "chat",
+  password: ""
+}); 
 
 // WebSocket-сервер на порту 8081
 var webSocketServer = new WebSocketServer.Server({ port: 8081 });
@@ -16,7 +23,9 @@ webSocketServer.on('connection', function (ws) {
 
   ws.on('message', function (message) {
     console.log('получено сообщение ' + message);
-    let arr = JSON.parse(message);    
+    let arr = JSON.parse(message);
+    const query = "INSERT INTO messages (nick, message) VALUES (?,?)";
+    sqlConnection.query(query, [arr.nick, arr.message])
 
     for (var key in clients) {
       clients[key].send(message);
@@ -26,7 +35,6 @@ webSocketServer.on('connection', function (ws) {
   ws.on('close', function () {
     console.log('соединение закрыто ' + id);
     delete clients[id];
-    delete clientAge[id];
   });
 });
 
@@ -34,9 +42,7 @@ webSocketServer.on('connection', function (ws) {
 // обычный сервер (статика) на порту 8080
 var fileServer = new Static.Server('.');
 http.createServer(function (req, res) {
-
   fileServer.serve(req, res);
-
 }).listen(8080);
 
 console.log("Сервер запущен на портах 8080, 8081");
