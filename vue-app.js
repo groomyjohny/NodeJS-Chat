0a53995beee40a61864a0229cdda1077bd9e5c4a
groomyjohny <groomyjohny@gmail.com>
2021-02-10 07:18:53 +0500

    Created renderMessage function in Vue app

--- vue-app.js
+++ vue-app.js
@@ -6,9 +6,7 @@ var app = new Vue({
     methods: {
         addMessage : function(msg, appendToFront)
         {
-            let searchResult = this.messages.find( (valInArr) => {
-                return valInArr.id == msg.id;
-            });
+            let searchResult = this.getMessageById(msg.id);
 
             if (searchResult)
             {
@@ -18,6 +16,34 @@ var app = new Vue({
 
             if (appendToFront) this.messages.unshift(msg);
             else this.messages.push(msg);
+        },
+
+        renderMessage : function(msg, offset = 0)
+        {
+            if (!msg) return "Ошибка: renderMessage вызвано с msg == "+msg;
+            const levelOffset = 1;
+            let s = '';
+            for (let i = 0; i < offset; ++i) s += '<div class="message-reply-spacer"/>';
+            s += `<div class="message-id">${msg.id}</div>
+            <div class="message-datetime">${msg.datetime}</div>
+            <div class="message-nick">${msg.nick}</div>`;
+            if (msg.replyList)
+            {
+                for (item in replyList) 
+                {
+                    s += this.renderMessage(this.getMessageById(msg[item].id),offset + levelOffset)
+                }
+            }
+            s += `<div class="message-text">${msg.message}</div>
+            <a class="message-reply-link">Ответить</a>`;
+            return s;
+        },
+
+        getMessageById : function(id)
+        {
+            return this.messages.find( (valInArr) => {
+                return valInArr.id == id;
+            });
         }
     }
   })
